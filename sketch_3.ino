3edba10bccf47064311b076cabf601547560da83
anTONIazupa <69693177+anTONIazupa@users.noreply.github.com>
2020-08-14 17:30:49 -0400

    Add files via upload

--- sketch_3.ino
+++ sketch_3.ino
@@ -1,19 +1,20 @@
-int x;
-int y;
+int x; //number of cattle in the pen
+int y; //number of cattle that should be in the pen
 int ledPin = 11;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
+  Serial.print("Please insert the number of cattle in the pen and the number of cattle that should be in the pen");
 }
 
 void loop() {
   if (Serial.available()) {
     x = Serial.parseInt();
     y = Serial.parseInt();
-    Serial.print("\nxreceived: ");
+    Serial.print("\nnumber of cattle in the pen: ");
     Serial.println(x);
-       Serial.print("\nyreceived: ");
+       Serial.print("\nnumber of cattle that should be in the pen: ");
     Serial.println(y);
   }{    
     if (x==y)

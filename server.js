58a9be3bf147ccebfc04de26b5c1398eccf9339a
Zachflah <47955102+Zachflah@users.noreply.github.com>
2020-08-14 21:07:10 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,8 @@
-/*
-const opn = require('opn');
-
-var express = require("express");
-
-var app = express();
-
-app.use(express.static('public'));
-
-//make way for some custom css, js and images
-app.use('/css', express.static(__dirname + '/public/css'));
-app.use('/js', express.static(__dirname + '/public/js'));
-app.use('/images', express.static(__dirname + '/public/images'));
-
-var server = app.listen(8081, function(){
-    var port = server.address().port;
-    console.log("Server started at http://localhost:%s", port);
-
-    opn('http://localhost:8080')
-      .catch(() => {});
-});
-*/
-
 const http = require("http");
 const fs = require("fs");
 var express = require("express");
 var cors = require("cors");
-const { track } = require("./Tracking")
+//const { track } = require("./Tracking")
 const hostname = "127.0.0.1";
 const port = 3000;
 
@@ -50,17 +27,15 @@ app.get("/", (req, res) => {
   });
 });
 
-app.post("/auth", (req, res) => {
-  console.log(req.body);
+/*
+app.post("/track", (req, res) => {
 
-  var user = req.body.user;
-  var pass = req.body.pass;
 
-  // If info correct send redirect
+
   track(user, pass);
-  window.location.replace("https://www.mess-segm.ca/test-bank");
-});
 
+});
+*/
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 //app.redirect('http://localhost:3000')
